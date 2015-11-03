118a988a64a8f5c30281070fd485bff5ee138732
mackeprm <github@zvaadw.de>
2015-11-03 10:38:26 +0100

    * fixed bug in bubblesort that created multiple Skandium Instances

--- examples/src/main/java/cl/niclabs/skandium/examples/bubblesort/BubbleSort.java
+++ examples/src/main/java/cl/niclabs/skandium/examples/bubblesort/BubbleSort.java
@@ -18,13 +18,14 @@
 
 package cl.niclabs.skandium.examples.bubblesort;
 
+import cl.niclabs.skandium.Skandium;
+import cl.niclabs.skandium.Stream;
+import cl.niclabs.skandium.skeletons.For;
+import cl.niclabs.skandium.skeletons.Skeleton;
+
 import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.Future;
-
-import cl.niclabs.skandium.Skandium;
-import cl.niclabs.skandium.skeletons.For;
-import cl.niclabs.skandium.skeletons.Skeleton;
 
 public class BubbleSort {
 	static Random random = new Random();
@@ -32,7 +33,7 @@ public class BubbleSort {
 	public static void main(String[] args) {
 
 		int THREADS  = Runtime.getRuntime().availableProcessors();
-		int SIZE     = (int) Math.pow(2, 26);
+        int SIZE = (int) Math.pow(2, 15);
 
 		if(args.length != 0) {
 			THREADS  = Integer.parseInt(args[0]);
@@ -44,34 +45,36 @@ public class BubbleSort {
 
 		System.out.println("Computing BubbleSort threads="+THREADS+" size="+ SIZE + ".");
 
-		Skeleton<ArrayList<Integer>, ArrayList<Integer>> bsort = new For<ArrayList<Integer>>(
-				new BSExecute(), in.size());
+        Skeleton<ArrayList<Integer>, ArrayList<Integer>> bsort = new For<>(
+                new BSExecute(), in.size());
 
 		Skandium skandium = new Skandium(THREADS);
 		
 		long init = System.currentTimeMillis();
-		
-		Future<ArrayList<Integer>> future = bsort.input(in);
-		try {
-			out = future.get();
+
+        Stream<ArrayList<Integer>, ArrayList<Integer>> stream = skandium.newStream(bsort);
+        Future<ArrayList<Integer>> future = stream.input(in);
+        try {
+            out = future.get();
 			System.out.println((System.currentTimeMillis() - init)+"[ms]: "+out.toArray());
 			for(int i=0; i<out.size()-2;i++) {
 				if(out.get(i) > out.get(i+1)) throw new Exception("Not sorted! "+ i + " "+ (i+1));
      			}
-			
-			skandium.shutdown();
-		} catch (Exception e) {
+
+            System.out.println("Checked array.");
+            skandium.shutdown();
+        } catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
         
 	public static ArrayList<Integer> generate(int size){
 
-		ArrayList<Integer> array = new ArrayList<Integer>(size);
+        ArrayList<Integer> array = new ArrayList<>(size);
 
 		for(int i=0;i<size;i++){
-			array.add(i, new Integer(random.nextInt()));
-		}
+            array.add(i, random.nextInt());
+        }
 
 		return array;
 	}
