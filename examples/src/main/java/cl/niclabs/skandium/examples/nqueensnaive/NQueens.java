b6d8c2ba8915d00b74d34234abd28361751d2c92
mackeprm <github@zvaadw.de>
2015-11-13 10:30:03 +0100

    * altered output format

--- examples/src/main/java/cl/niclabs/skandium/examples/nqueensnaive/NQueens.java
+++ examples/src/main/java/cl/niclabs/skandium/examples/nqueensnaive/NQueens.java
@@ -17,67 +17,67 @@
  */
 package cl.niclabs.skandium.examples.nqueensnaive;
 
+import cl.niclabs.skandium.Skandium;
+import cl.niclabs.skandium.Stream;
+import cl.niclabs.skandium.skeletons.DaC;
+import cl.niclabs.skandium.skeletons.Map;
+import cl.niclabs.skandium.skeletons.Skeleton;
+
 import java.util.Random;
 import java.util.concurrent.Future;
 
-import cl.niclabs.skandium.skeletons.Map;
-import cl.niclabs.skandium.skeletons.DaC;
-import cl.niclabs.skandium.skeletons.Skeleton;
-import cl.niclabs.skandium.Skandium;
-import cl.niclabs.skandium.Stream;
-
 /**
  * The main class to execute a naive NQueens counting algorithm which does not consider board symmetries.
- * 
+ *
  * @author mleyton
  */
-public class NQueens{
+public class NQueens {
 
-	static Random random = new Random();
-	
+    static Random random = new Random();
+
     public static void main(String[] args) throws Exception {
-       
-    	int THREADS = Runtime.getRuntime().availableProcessors();
-    	int BOARD = 15;  //Size board of the board
-    	int DEPTH = 3;
-    	
-    	if(args.length != 0) {
-    		THREADS = Integer.parseInt(args[0]);
-    		BOARD   = Integer.parseInt(args[1]);
-    		DEPTH   = Integer.parseInt(args[2]);
-    	}
-    	
-    	System.out.println("Computing NQueens threads="+THREADS+" board="+ BOARD+" depth="+DEPTH+ ".");
-    	
-    	//1. Define the skeleton program structure
-    	 Skeleton<Board, Count> subskel = new DaC<Board, Count>(   //We use a divide and conquer skeleton pattern
-    			 new ShouldDivide(DEPTH),  //Dive until the depth is "N-3" 
-    			 new DivideBoard(), 
-    			 new Solve(), 
-    			 new ConquerCount());
-    	
-    	 Skeleton<Board, Count> nqueens = //Always subdivide the first row.
-    		 new Map<Board, Count>(new DivideBoard(), subskel, new ConquerCount());
-    	 
-    	 //2. Create a new Skandium instance with 2 execution threads
-         Skandium skandium = new Skandium(THREADS);
 
-         //3. Open a Stream to input parameters
-         Stream<Board, Count> stream = skandium.newStream(nqueens);
-         
-         //4. Input parameters
-         long init = System.currentTimeMillis();
-         Future<Count> future = stream.input(new Board(BOARD));
+        int THREADS = Runtime.getRuntime().availableProcessors();
+        int BOARD = 15;  //Size board of the board
+        int DEPTH = 3;
 
-         //5. Do something else here.
-         //...
-         
-         //6. Block for the results
-         Count result = future.get();
-         System.out.println(result+" in "+(System.currentTimeMillis() - init)+"[ms]");
-         
-         //7. Shutdown the system
-         skandium.shutdown();
-         
+        if (args.length != 0) {
+            THREADS = Integer.parseInt(args[0]);
+            BOARD = Integer.parseInt(args[1]);
+            DEPTH = Integer.parseInt(args[2]);
+        }
+
+        System.out.println("Computing NQueens threads=" + THREADS + " board=" + BOARD + " depth=" + DEPTH + ".");
+
+        //1. Define the skeleton program structure
+        Skeleton<Board, Count> subskel = new DaC<Board, Count>(   //We use a divide and conquer skeleton pattern
+                new ShouldDivide(DEPTH),  //Dive until the depth is "N-3"
+                new DivideBoard(),
+                new Solve(),
+                new ConquerCount());
+
+        Skeleton<Board, Count> nqueens = //Always subdivide the first row.
+                new Map<Board, Count>(new DivideBoard(), subskel, new ConquerCount());
+
+        //2. Create a new Skandium instance with 2 execution threads
+        Skandium skandium = new Skandium(THREADS);
+
+        //3. Open a Stream to input parameters
+        Stream<Board, Count> stream = skandium.newStream(nqueens);
+
+        //4. Input parameters
+        long init = System.currentTimeMillis();
+        Future<Count> future = stream.input(new Board(BOARD));
+
+        //5. Do something else here.
+        //...
+
+        //6. Block for the results
+        Count result = future.get();
+        System.out.println("time:" + +(System.currentTimeMillis() - init) + "[ms]");
+        System.out.println(result);
+        //7. Shutdown the system
+        skandium.shutdown();
+
     }
 }
